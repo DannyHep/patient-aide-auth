994e9b0bb27df99ec597687867ed7ef81c866630
Rafael Lima <rafauxdev@gmail.com>
2022-08-03 14:13:07 +0100

    Add Azure CosmosDB

--- config/db.js
+++ config/db.js
@@ -1,13 +1,32 @@
 require("dotenv").config();
 
 const mongoose = require("mongoose");
+const mongooseCS = require("mongoose");
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connected");
-  })
-  .catch((error) => console.log(error));
+const { DB_ENV, COSMOSDB_USER, COSMOSDB_PASSWORD, COSMOSDB_URI, MONGODB_URI } =
+  process.env;
+
+if (DB_ENV === "AZURE") {
+  mongooseCS
+    .connect(COSMOSDB_URI, {
+      auth: {
+        username: COSMOSDB_USER,
+        password: COSMOSDB_PASSWORD,
+      },
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      retryWrites: false,
+    })
+    .then(() => console.log("Connection to CosmosDB successful"))
+    .catch((err) => console.error(err));
+} else {
+  mongoose
+    .connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Mongo DB Connected");
+    })
+    .catch((error) => console.log(error));
+}
